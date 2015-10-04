9444c9b570860331b2928325163c51336fb6facb
kuknina <nina.kuklisova@gmail.com>
2015-10-04 18:29:52 -0400

    Create load_data_lake.sh

---
+++ loading_and_modeling/load_data_lake.sh
@@ -0,0 +1,4 @@
+wget https://s3-us-west-1.amazonaws.com/bucket147/effective_care.csv
+wget https://s3-us-west-1.amazonaws.com/bucket147/hospitals.csv
+wget https://s3-us-west-1.amazonaws.com/bucket147/readmissions.csv
+wget https://s3-us-west-1.amazonaws.com/bucket147/surveys_responses.csv
