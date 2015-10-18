4ea2b79aa9a41ca8792839a01139b8793b46ee64
kuknina <nina.kuklisova@gmail.com>
2015-10-18 13:12:22 -0400

    Update hive_base_ddl.sql

--- loading_and_modeling/hive_base_ddl.sql
+++ loading_and_modeling/hive_base_ddl.sql
@@ -1,23 +1,44 @@
-CREATE EXTERNAL TABLE effective_care ('Provider_ID', 'Hospital_Name', 'Address', 'City', 'State', 'ZIP_Code', 'County_Name', 'Phone_Number', 'Condition', 'Measure_ID', 'Measure_Name', 'Score', 'Sample', 'Footnote')
-ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY ‘,’ 
-STORED AS TEXTFILE 
-LOCATION ‘/user/kuknina/hospital_compare’;
+DROP TABLE effective_care;
 
-CREATE EXTERNAL TABLE hospitals ('Provider_ID', 'Hospital_Name', 'Address', 'City', 'State', 'ZIP_Code', 'County_Name', 'Phone_Number', 'Hospital_Type', 'Hospital_Ownership', 'Emergency_Services')
+CREATE EXTERNAL TABLE effective_care (Provider_ID varchar(500), Hospital_Name varchar(500), Address varchar(500), City varchar(500), State varchar(500), ZIP_Code varchar(500), County_Name varchar(500), Phone_Number varchar(500), Condition varchar(500), Measure_ID varchar(500), Measure_Name varchar(500), Score varchar(500), Sample varchar(500), Footnote varchar(500))
 ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY ‘,’ 
-STORED AS TEXTFILE 
-LOCATION ‘/user/kuknina/hospital_compare’;
+FIELDS TERMINATED BY '\t' 
+STORED AS TEXTFILE
+LOCATION '/user/kuknina/hospital_compare';
 
-CREATE EXTERNAL TABLE readmissions ('Provider_ID', 'Hospital_Name', 'Address', 'City', 'State', 'ZIP_Code', 'County_Name', 'Phone_Number', 'Measure_Name', 'Measure_ID', 'Compared_to_National', 'Score', 'Lower_Estimate', 'Higher_Estimate', 'Footnote', 'Measure_Start_Date', 'Measure_End_Date')
-ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY ‘,’ 
-STORED AS TEXTFILE 
-LOCATION ‘/user/kuknina/hospital_compare’;
+LOAD DATA LOCAL INPATH 'effective_care.csv.2'
+OVERWRITE INTO TABLE effective_care;
 
-CREATE EXTERNAL TABLE surveys_responses ('Provider_ID', 'Hospital_Name', 'Address', 'City', 'State', 'ZIP_Code', 'County_Name', 'Phone_Number', 'Condition', 'Measure_ID', 'Measure_Name', 'Score', 'Sample', 'Footnote', 'Measure_Start_Date', 'Measure_End_Date')
+DROP TABLE hospital_compare;
+
+CREATE EXTERNAL TABLE hospital_compare (Provider_ID varchar(500), Hospital_Name varchar(500), Address varchar(500), City varchar(500), State varchar(500), ZIP_Code varchar(500), County_Name varchar(500), Phone_Number varchar(500), Hospital_Type varchar(500),  Hospital_Ownership varchar(500), Emergency_Services varchar(500))
 ROW FORMAT DELIMITED 
-FIELDS TERMINATED BY ‘,’ 
-STORED AS TEXTFILE 
-LOCATION ‘/user/kuknina/hospital_compare’;
+FIELDS TERMINATED BY '\t' 
+STORED AS TEXTFILE
+LOCATION '/user/kuknina/hospital_compare';
+
+LOAD DATA LOCAL INPATH 'hospitals.csv'
+OVERWRITE INTO TABLE hospital_compare;
+
+
+DROP TABLE readmissions;
+
+CREATE EXTERNAL TABLE readmissions (Provider_ID varchar(500), Hospital_Name varchar(500), Address varchar(500), City varchar(500), State varchar(500), ZIP_Code varchar(500), County_Name varchar(500), Phone_Number varchar(500), Measure_Name varchar(500), Measure_ID varchar(500), Compared_to_National varchar(500), Score varchar(500), Lower_Estimate varchar(500), Higher_Estimate varchar(500), Footnote varchar(500), Measure_Start_Date varchar(500), Measure_End_Date varchar(500))
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY '\t' 
+STORED AS TEXTFILE
+LOCATION '/user/kuknina/hospital_compare';
+
+LOAD DATA LOCAL INPATH 'readmissions.csv'
+OVERWRITE INTO TABLE readmissions;
+
+DROP TABLE surveys_responses;
+
+CREATE EXTERNAL TABLE surveys_responses (Provider_ID varchar(500), Hospital_Name varchar(500), Address varchar(500), City varchar(500), State varchar(500), ZIP_Code varchar(500), County_Name varchar(500), Phone_Number varchar(500), Condition varchar(500), Measure_ID varchar(500), Measure_Name varchar(500), Score varchar(500), Sample varchar(500), Footnote varchar(500), Measure_Start_Date varchar(500), Measure_End_Date varchar(500))
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY '\t' 
+STORED AS TEXTFILE
+LOCATION '/user/kuknina/hospital_compare';
+
+LOAD DATA LOCAL INPATH 'surveys_responses.csv'
+OVERWRITE INTO TABLE surveys_responses;
